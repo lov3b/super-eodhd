7a831cf7674b7933f990ac474cdbad21b8835508
Love Billenius <lovebillenius@disroot.org>
2024-02-13 13:15:29 +0100

    add custom serializer/deserializer for datetime

--- src/models.rs
+++ src/models.rs
@@ -1,24 +1,38 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Intraday {
-    #[serde(rename = "Timestamp")]
     pub timestamp: i64,
-    #[serde(rename = "Gmtoffset")]
+    #[serde(rename = "gmtoffset")]
     pub gmt_offset: i32,
-    #[serde(rename = "Datetime")]
+    #[serde(
+        deserialize_with = "deserialize_datetime",
+        serialize_with = "serialize_datetime"
+    )]
     pub datetime: DateTime<Utc>,
-    #[serde(rename = "Open")]
     pub open: f64,
-    #[serde(rename = "High")]
     pub high: f64,
-    #[serde(rename = "Low")]
     pub low: f64,
-    #[serde(rename = "Close")]
     pub close: f64,
-    #[serde(rename = "Volume")]
     pub volume: i64,
+}
+fn deserialize_datetime<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let naivedt =
+        NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").map_err(serde::de::Error::custom)?;
+    Ok(DateTime::from_naive_utc_and_offset(naivedt, Utc))
+}
+
+fn serialize_datetime<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let s = date.format("%Y-%m-%d %H:%M:%S").to_string();
+    serializer.serialize_str(&s)
 }
 
 #[derive(Serialize, Deserialize, Debug)]
